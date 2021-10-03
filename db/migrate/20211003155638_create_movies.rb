14056a1aee2e716b67aa0acf9807704b3415c27b
Eric Nganga <ericmainaedits@gmail.com>
2021-10-03 11:09:37 -0500

    create database

--- db/migrate/20211003155638_create_movies.rb
+++ db/migrate/20211003155638_create_movies.rb
@@ -3,7 +3,7 @@ class CreateMovies < ActiveRecord::Migration[6.1]
     create_table :movies do |t|
       t.string :title
       t.string :plot
-      t.int :rating
+      t.integer :rating
 
       t.timestamps
     end
