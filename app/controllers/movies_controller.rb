7d50e26ad4b56569403b7f8e86820ce6c23c43bc
Eric Nganga <ericmainaedits@gmail.com>
2021-10-04 20:28:17 -0500

    add movie index and show routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,5 +5,13 @@ class MoviesController < ApplicationController
         render json: movies
     end
 
-    
+    def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      render json: movie
+    else
+      render json: { error: "Movie not found" }, status: :not_found
+    end
+  end
+
 end
