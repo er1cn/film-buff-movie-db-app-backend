bf51adb6550bb11a39631386ec3bfdd5051c58d0
Eric Nganga <ericmainaedits@gmail.com>
2021-10-04 22:31:45 -0500

    add movie serializer

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'puma', '~> 5.0'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
 gem 'dotenv-rails'
-
+gem 'fast_jsonapi'
 gem 'rest-client'
 gem 'json'
 gem 'pry'
@@ -30,3 +30,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,16 +2,17 @@ class MoviesController < ApplicationController
 
     def index
         movies = Movie.all
-        render json: movies
+        render json: MovieSerializer.new(movies) 
     end
 
     def show
     movie = Movie.find_by(id: params[:id])
     if movie
-      render json: movie
+      render json: MovieSerializer.new(movie) 
     else
       render json: { error: "Movie not found" }, status: :not_found
     end
   end
+
 
 end
