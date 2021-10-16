c88d597ade1f37de8e56234f0dd5d61769df6bc6
Eric Nganga <ericmainaedits@gmail.com>
2021-10-15 21:51:17 -0500

    signup route edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
      resources :ratings, only: [:show, :index]
   end
   
-  post "/signup" to: "users#create"
+  post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
   delete "/logout", to: "sessions#destroy"
